9101c5e9e31284149ae8f34ba29c900756283e62
Rintia Islam <rintiaislam110@gmail.com>
2023-11-27 00:22:25 +0600

    footer addedd

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Shared/Navbar';
+import Footer from '../Shared/Footer';
 
 const Main = () => {
     return (
         <div className='max-w-screen-xl mx-auto'>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
