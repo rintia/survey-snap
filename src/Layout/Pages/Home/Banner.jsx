659312369c036f3eca2c603942e9af2cd7681b8c
Rintia Islam <rintiaislam110@gmail.com>
2023-11-26 16:39:07 +0600

    banner added

--- src/Layout/Pages/Home/Banner.jsx
+++ src/Layout/Pages/Home/Banner.jsx
@@ -2,13 +2,13 @@
 
 const Banner = () => {
     return (
-        <div className="relative min-h-[600px] mb-32" style={{backgroundImage: 'url(https://i.ibb.co/wzVs3GR/banner.jpg)'}}>
-        <div className="hero-overlay  bg-opacity-60"></div>
-        <div className=" absolute top-20 left-10 text-left text-neutral-content">
+        <div  className=" hero relative min-h-[600px] mb-32" style={{backgroundImage: 'url(https://i.ibb.co/bdjMWd7/kk.jpg)', backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
+        <div className="hero-overlay  bg-opacity-80"></div>
+        <div className=" hero-content text-center text-neutral-content">
           <div className="max-w-md ">
-            <h1 className="mb-5 text-[#ED7D31] text-7xl font-bold">Your Partner For Online Job Market</h1>
-            <p className="mb-5">Get your work done by the right person. And post your job right here, right now</p>
-            <button className="btn btn-outline text-[#ED7D31]">Get Started</button>
+            <h1 className="mb-5 text-[#61B15A] text-7xl font-bold">Find All The Surveys In One Place</h1>
+            <p className="mb-5">Participate on different surveys all around the world</p>
+            <button className="btn btn-outline text-[#61B15A]">Explore</button>
           </div>
         </div>
       </div>
