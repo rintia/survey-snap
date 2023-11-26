fb0fb69485aaf917eb7e0f97a0c4f29e9bc01199
Rintia Islam <rintiaislam110@gmail.com>
2023-11-26 23:26:49 +0600

    survey filter added

--- src/Layout/Pages/AllSurveys/AllSurveys.jsx
+++ src/Layout/Pages/AllSurveys/AllSurveys.jsx
@@ -1,21 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useAxiosPublic from '../../../hooks/useAxiosPublic';
 import { useQuery } from '@tanstack/react-query';
 import SurveyCard from './SurveyCard';
 
 const AllSurveys = () => {
+    let surveys = []
+    const [selectedFilter, setSelectedFilter] = useState('');
     const axiosPublic = useAxiosPublic();
-    const { data: surveys = [], refetch } = useQuery({
+    const { data: initialSurveys = [], refetch } = useQuery({
         queryKey: ['surveys'],
         queryFn: async () => {
             const res = await axiosPublic.get('/surveys');
             return res.data;
+            
         }
-    })
-    console.log(surveys);
+    });
+    surveys = initialSurveys;
+
+    const handleFilterChange = (e) => {
+        const selectedValue = e.target.value;
+        setSelectedFilter(selectedValue);
+    
+        // Perform actions based on the selected option
+        switch (selectedValue) {
+          case 'Title':
+            handleTitleSelect();
+            break;
+          case 'Category':
+            handleCategorySelect();
+            break;
+        //   case 'TotalVote':
+        //     handleTotalVoteSelect();
+        //     break;
+          default:
+            setSelectedFilter('Filter By')
+            // Handle default case or perform no action
+        }
+      };
+    
+   const handleTitleSelect = () =>{
+       const sorted = surveys.sort((a, b) => {
+        return a.title.localeCompare(b.title);
+    });
+    surveys =sorted
+   }
+   const  handleCategorySelect = () =>{
+       const sorted = surveys.sort((a, b) => {
+        return a.category.localeCompare(b.category);
+    });
+    surveys =sorted
+   }
     return (
         <div>
-            
+           
+           <select className=" mt-8 select select-success  max-w-xs"
+            value={selectedFilter}
+            onChange={handleFilterChange}>
+                <option selected>Filter By</option>
+                <option>Title</option>
+                <option>Category</option>
+                <option>Total Vote</option>
+            </select>
+           
             <div className='my-12 px-4 lg:px-0 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
                 {
                     surveys.map(survey => <SurveyCard key={survey._id} survey={survey}></SurveyCard>)
